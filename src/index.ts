7492a5e35521f958806bd12c7fd77f36a0ea7c26
MaxwellChrist <ribthulu@gmail.com>
2022-10-31 10:59:46 -0400

    figured out how to add tasks to local storage

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,14 @@ let counter: number = 1;
 const list = document.getElementById("list") as HTMLUListElement | null;
 const listForm = document.querySelector<HTMLFormElement>("#new-task-form");
 const listTitle = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
 
 type Task = {
   id: number,
   title: string,
   completed: boolean,
   createdAt: Date,
-}
-
-function addListItem(task: Task) {
-  const item = document.createElement("li")
-  const label = document.createElement("label");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox"
-  label.append(checkbox, task.title)
-  item.append(label)
-  list?.append(item)
 }
 
 listForm?.addEventListener("submit", e => {
@@ -31,6 +23,34 @@ listForm?.addEventListener("submit", e => {
     completed: false,
     createdAt: new Date()
   }
-  counter++
+  tasks.push(task)
+  saveTasks()
   addListItem(task) 
+  counter++
+  listTitle.value = ""
 })
+
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+  const JSONTask = localStorage.getItem("TASKS")
+  if (JSONTask == null) return []
+  return JSON.parse(JSONTask)
+}
